66383be37291a73043e7f32656cc454a7a1105b9
emmanuel-odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2025-01-20 19:16:55 +0000

    security lab added

---
+++ week2_labs/advance_security/src/test/java/advance_security/security/SecurityApplicationTests.java
@@ -0,0 +1,12 @@
+package advance_security.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+    @Test
+    void runTest () {
+        System.out.println( "true = " + true );
+    }
+}
